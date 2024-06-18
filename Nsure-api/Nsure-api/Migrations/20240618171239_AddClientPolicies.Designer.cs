415cf1b7b3ec880550ab07414616c0999ef4ddea
Josh Hill <148567526+Sahujo-Hill@users.noreply.github.com>
2024-06-18 18:18:36 +0100

    Updated Database
    Add-Migration AddClientPolicies
    Update-Database
    
    To add new cs items

--- Nsure-api/Nsure-api/Migrations/20240618171239_AddClientPolicies.Designer.cs
+++ Nsure-api/Nsure-api/Migrations/20240618171239_AddClientPolicies.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Nsure.api.Nsure.Models;
 
@@ -11,9 +12,10 @@ using Nsure.api.Nsure.Models;
 namespace Nsure_api.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240618171239_AddClientPolicies")]
+    partial class AddClientPolicies
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Nsure-api/Nsure-api/Migrations/20240618171239_AddClientPolicies.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Nsure_api.Migrations
+{
+    public partial class AddClientPolicies : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "policies",
+                table: "Clients");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "carPolicy",
+                table: "Clients",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "homePolicy",
+                table: "Clients",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "lifePolicy",
+                table: "Clients",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "carPolicy",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "homePolicy",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "lifePolicy",
+                table: "Clients");
+
+            migrationBuilder.AddColumn<string>(
+                name: "policies",
+                table: "Clients",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
